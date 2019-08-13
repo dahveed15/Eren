b4a1d6a81e5318c86f73318263479c96099d222c
David Harris <david.harris@omadahealth.com>
2019-08-13 09:55:48 -0700

    add 'Add Machine' link with styles
    Signed-off-by: Peter Vliet <peter.vliet@omadahealth.com>

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Splash page', type: :feature, js: true do
   scenario 'index page' do
     visit root_path
     expect(page).to have_content('Welcome to Eren, the best app ever!')
-    find_button('Add Machine').click
+    find_link('Add Machine').click
     expect(page).to have_content('Add a Machine')
   end
 end
