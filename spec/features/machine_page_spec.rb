fdf988274cb9f322769c3f20c1e69b45c3ba17cc
David Harris <dahveed15@gmail.com>
2019-08-20 09:39:14 -0700

    Create Machine functionality
    Signed-off-by: Peter Vliet <peter.vliet@omadahealth.com>

--- spec/features/machine_page_spec.rb
+++ spec/features/machine_page_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe 'Add Machine page', type: :feature, js: true do
   scenario 'index page' do
     visit new_machines_path
-    fill_in('Name: ', :with => 'Lat Pulldown')
+    fill_in('machine[name]', :with => 'Lat Pulldown')
     find_button('Create Machine').click
-    
+    expect(page).to have_content('Lat Pulldown')
   end
 end
