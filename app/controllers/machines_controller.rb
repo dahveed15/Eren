9683851dd9c31b9379aaefa334d093e5baa3362d
David Harris <david.harris@omadahealth.com>
2019-08-20 09:39:14 -0700

    Create Machine functionality
    Signed-off-by: Peter Vliet <peter.vliet@omadahealth.com>

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,6 +1,19 @@
 class MachinesController < ApplicationController
 
   def new
+    @machine = Machine.new
   end
 
+  def create
+    @machine = Machine.new(machine_params)
+    if @machine.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def machine_params
+    params.require(:machine).permit(:name)
+  end
 end
