8f9e944567d30915b7580557c24022c514fa3224
David Harris <david.harris@omadahealth.com>
2019-09-10 09:56:09 -0700

    Add a 'Add new Exercise' link with its respective page
    Signed-off-by: Peter Vliet <peter.vliet@omadahealth.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # get 'dashboards/show'
 
   root to: 'home#show'
-  resource :machines, only: [:new, :create]
+  resource :machines, only: [:new, :create] do
+    resource :exercises, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe 'Splash page', type: :feature, js: true do
     find_link('Add Machine').click
     expect(page).to have_content('Add a Machine')
   end
+
+  context 'with a machine created' do
+    let!(:benchpress_machine) { Machine.create(name: 'benchpress') }
+
+    scenario 'add an exercise' do
+      visit root_path
+      expect(page).to have_content(benchpress_machine.name)
+      find_link('Add Exercise').click
+      expect(page).to have_content('Add an Exercise for benchpress')
+    end
+  end
 end
