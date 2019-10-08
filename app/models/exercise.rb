d6070fac88f10bcaa15c115e21e54b80c0d06013
Peter Vliet <peter.vliet@omadahealth.com>
2019-10-08 09:15:14 -0700

    Add annotate gem to project
    Signed-off-by: David Harris <david.harris@omadahealth.com>

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: exercises
+#
+#  id           :bigint           not null, primary key
+#  reps         :integer          not null
+#  sets         :integer
+#  weight_value :integer          not null
+#  machine_id   :bigint
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  units        :string
+#
+
 class Exercise < ApplicationRecord
   belongs_to :machine
   POSSIBLE_WEIGHT_UNITS = ['lbs', 'kg', 'units']
