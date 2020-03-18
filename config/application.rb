4ab512e259e0fe787a40bb7295f36cde84c4f26e
David Harris <dahveed15@gmail.com>
2020-03-18 09:22:28 -0700

    Change autoload path to eager load path Allow ChangeExerciseMachineIdToActivityId to be called in the heroku rails production console
    Signed-off-by: Peter Vliet <peter.vliet@omadahealth.com>

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,6 @@ module Eren
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.autoload_paths << Rails.root.join('db/data_migrations').to_s
+    config.eager_load_paths << Rails.root.join('db/data_migrations').to_s
   end
 end
